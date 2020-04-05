7069e74918b1b0661a10e3ae1fd6e9e7f5165df3
bianca <bianca.hinova@gmail.com>
2020-04-05 23:21:46 +0300

    project initial configuration

---
+++ qpon-ios/qpon-ios/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  qpon-ios
+//
+//  Created by Bianca Hinova on 5.04.20.
+//  Copyright © 2020 Bianca Hinova. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
---
+++ qpon-ios/qpon-ios/SplashLoadingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  qpon-ios
+//
+//  Created by Bianca Hinova on 5.04.20.
+//  Copyright © 2020 Bianca Hinova. All rights reserved.
+//
+
+import UIKit
+
+class SplashLoadingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // TODO: determine if user is logged in
+        // navigate to login or home
+        self.navigateToLogin()
+    }
+    
+    func navigateToLogin() {
+        let loginVC = LoginViewController(nibName: "LoginViewController", bundle: nil)
+        loginVC.modalPresentationStyle = .fullScreen
+        self.present(loginVC, animated: true, completion: nil)
+    }
+    
+    func navigateToHome() {
+        
+    }
+}
+
